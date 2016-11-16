f01f92c2e532641a1a446f7c1ea4d6c48cbc1130
lagwy <lagwyf@gmail.com>
2016-11-15 18:25:58 -0600

    Footer added to every file

---
+++ js/footer.js
@@ -0,0 +1,2 @@
+var foot = "2016 © Abastecimientos y Equipos Informáticos S. de R.L. de C.V. Todos los derechos reservados.";
+$("#footer").append('<div class="container"><div class="row"><div class="col-xs-12"><p>' + foot + '</p></div></div></div>');
