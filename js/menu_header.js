17d0d148d13c2a494d0020f699839c39af01a3c7
lagwy <lagwyf@gmail.com>
2016-11-16 19:06:42 -0600

    Header changes

--- js/menu_header.js
+++ js/menu_header.js
@@ -1,4 +1,7 @@
- var text = "";
+var logo_header = '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"><span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span></button>  <a class="navbar-brand" href="home.html">Abastecimientos y Equipos Informáticos</a>';
+$('#logo_header').append(logo_header);
+
+var text = "";
      text += '<ul class="nav navbar-nav navbar-right hidden-sm">';
      text += '<li><a href="catalog.html">Catálogo</a></li>';
     text += '<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"';
